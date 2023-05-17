b5801b94ca750e8d597bed7e191c3c34a8273366
wsy <wsy2161826815@163.com>
2023-05-17 22:02:16 +0800

    add ppt

--- USER/main.c
+++ USER/main.c
@@ -5,12 +5,12 @@ int main(void)
 {
   peripheral_init();
 
+  //task_dispatch_looper();
   fsm_t m_fsm;
   fsm_init(&m_fsm);
   while(1){
 		task_fsm_looper(&m_fsm);
   }
-
-  //task_dispatch_looper();
+  
 }
 
